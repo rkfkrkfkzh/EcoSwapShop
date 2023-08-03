ec51c1764b64db3eec91507a129eff2b2ae7ae80
imhyojin <1004dlagywls@naver.com>
2023-08-03 15:07:41 +0900

    회원가입,중복회원가입,회원삭제 test 검증완료

--- src/test/java/shop/ecoswapshop/service/MemberServiceTest.java
+++ src/test/java/shop/ecoswapshop/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import shop.ecoswapshop.domain.Address;
@@ -14,7 +15,6 @@ import shop.ecoswapshop.repository.MemberRepository;
 
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
@@ -26,6 +26,7 @@ public class MemberServiceTest {
     MemberRepository memberRepository;
 
     @Test
+    @Rollback(value = false)
     public void 회원가입() throws Exception {
         Member member = new Member();
         member.setUsername("john_doe");
@@ -44,10 +45,64 @@ public class MemberServiceTest {
         Assertions.assertNotNull(memberId);
         Assertions.assertTrue(memberId > 0);
 
-        // Verify that the member is saved in the repository
         Member savedMember = memberRepository.findById(memberId);
         Assertions.assertNotNull(savedMember);
         Assertions.assertEquals(member.getUsername(), savedMember.getUsername());
         Assertions.assertEquals(member.getEmail(), savedMember.getEmail());
+    }
+
+    @Test
+    public void 중복_회원가입() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setUsername("user1");
+        member1.setPassword("password");
+        member1.setFullName("Lim");
+        member1.setEmail("Lim.doe@example.com");
+        member1.setPhoneNumber("1234567890");
+        member1.setAddress(new Address("City", "Street", "12345"));
+        member1.setType(UserType.TYPE_USER);
+        member1.setRegistrationDate(LocalDateTime.now());
+
+        Member member2 = new Member();
+        member2.setUsername("user2");
+        member2.setPassword("password");
+        member2.setFullName("Lim");
+        member2.setEmail("kim.doe@example.com");
+        member2.setPhoneNumber("987654321");
+        member2.setAddress(new Address("City", "Street", "12345"));
+        member2.setType(UserType.TYPE_USER);
+        member2.setRegistrationDate(LocalDateTime.now());
+
+        //when
+        memberService.registerMember(member1);
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.registerMember(member2));
+    }
+
+    @Test
+    public void 회원_삭제() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("user1");
+        member.setPassword("password");
+        member.setFullName("Lim");
+        member.setEmail("Lim.doe@example.com");
+        member.setPhoneNumber("1234567890");
+        member.setAddress(new Address("City", "Street", "12345"));
+        member.setType(UserType.TYPE_USER);
+        member.setRegistrationDate(LocalDateTime.now());
+
+        //when
+        Long memberId = memberService.registerMember(member);
+
+        //then
+        Assertions.assertNotNull(memberId);
+
+        memberService.deleteMemberById(memberId);
+
+        Member deleteMember = memberService.findMemberById(memberId);
+        Assertions.assertNull(deleteMember);
     }
 }
