8df36cb4367197a8d65cae2009e05dc0c95a1450
imhyojin <1004dlagywls@naver.com>
2023-08-12 01:07:39 +0900

    SecurityConfig 에러.. 버전에 안맞나

--- src/main/java/shop/ecoswapshop/configuration/SecurityConfig.java
+++ src/main/java/shop/ecoswapshop/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/vertretungsplan").hasAnyRole("SCHUELER", "LEHRER", "VERWALTUNG")
+                        .mvcMatchers("/transplantable").hasAnyRole("SCHEDULER", "LEHRER", "UNWAVERING")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
